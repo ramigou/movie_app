a9c5101703f593679f811d6a6ce5fdd8ce0e6007
ramigou <dasul1021@gmail.com>
2021-07-29 17:24:38 +0900

    fin. added router and navigation

---
+++ src/components/Movie.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import "./Movie.css";
+
+// state가 필요 없으니까 그냥 함수로 작성
+function Movie({ id, title, year, summary, poster, genres }) {
+  return (
+    <Link
+      to={{
+        pathname: `/movie/${id}`,
+        state: {
+          year,
+          title,
+          summary,
+          poster,
+          genres
+        }
+      }}
+    >
+      <div className="movie">
+        <img src={poster} alt={title} title={title} />
+        <div className="movie__data">
+          <h3 className="movie_title">{title}</h3>
+          <h5 className="movie__year">{year}</h5>
+          <ul className="movie__genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="movie__summary">{summary.slice(0, 180)}...</p>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+
+export default Movie;
---
+++ src/routes/Detail.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+// 수동으로 영화 상세 페이지를 호출하면 메인 페이지로 리다이렉트
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return <span>{location.state.title}</span>;
+    } else {
+      return null;
+    }
+  }
+}
+
+export default Detail;
